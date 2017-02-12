7e27644752053faad4ce4e2f633d4849e27af010
Raiti <rusuke@t-net.ne.jp>
2017-02-12 13:25:57 +0900

    Add two module

---
+++ Compiler/src/test/CompilerTest.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * コンパイラテスト実行クラス。
+ * <br>Created by Raiti-chan on 2017/02/12.
+ *
+ * @author Raiti-chan
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class CompilerTest {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Parsing/src/test/ParsingTest.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * 構文解析テストクラス。
+ * <br>Created by Raiti-chan on 2017/02/12.
+ *
+ * @author Raiti-chan
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class ParsingTest {
+	public static void main(String[] args) {
+		
+	}
+}
